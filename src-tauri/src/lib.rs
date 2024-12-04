ebe67e9b9173dabf1eca12ae8a269efc11852835
Suresh Attanayake <sureshattanayake.a@affinidi.com>
2024-12-05 02:28:07 +0530

    surrealdb introduced

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,15 @@
 mod network;
 use std::sync::{Arc, RwLock};
-
 use network::network_interface::{get_net_ifaces, NetIface};
 use serde::Serialize;
+use surrealdb::engine::local::{Db, Mem};
+use surrealdb::Surreal;
 use tauri::State;
 
-#[derive(Default)]
+#[derive(Clone)]
 struct AppState {
     selected: Arc<RwLock<String>>,
+    db: Arc<RwLock<Surreal<Db>>>,
 }
 
 #[tauri::command]
@@ -32,9 +34,18 @@ pub struct NetLogEvent {
 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+pub async fn run() {
+
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("namespace").use_db("database").await.unwrap();
+
+    let app_state = AppState {
+        selected: Arc::new(RwLock::new("".to_string())),
+        db: Arc::new(RwLock::new(db))
+    };
+
     tauri::Builder::default()
-        .manage(AppState::default())
+        .manage(app_state)
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             get_network_interfaces,
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    argus_lib::run()
+#[tokio::main]
+async fn main() {
+    argus_lib::run().await;
 }
