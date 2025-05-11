2db388be83aa73dfd68d70cb7d77212862790d56
Suresh Attanayake <sureshattanayake.a@affinidi.com>
2025-05-11 12:47:23 +0200

    fixed the background image loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import bg from "./assets/main-bg.png";
+import bg from "./assets/main-bg.jpg";
 import AvailableNetworkInterface from "./Cards/AvailableNetworkInterfaces/AvailableNetworkInterface";
 import IPAddressesGraph from "./Cards/IPAddressesGraph/IPAddressesGraph";
 import LiveNetworkLogs from "./Cards/LiveNetworkLogs/LiveNetworkLogs";
@@ -10,8 +10,7 @@ import TrafficMap from "./Cards/TrafficMap/TrafficMap";
 
 function App() {
   return (
-    <main className="relative w-screen h-screen overflow-hidden  bg-black bg-cover xbg-[center_top_1rem]">
-      <img src={bg} alt="background" className="w-screen h-screen blur-sm" />
+    <main className="relative w-screen h-screen overflow-hidden  bg-black bg-cover xbg-[center_top_1rem]" style={{ backgroundImage: `url(${bg})` }}>
       <div className="absolute flex flex-col gap-3  top-0 left-0 w-screen h-screen bg-black/50 p-3">
         <div className="w-full h-1/2 flex gap-3">
           <div className="h-full w-2/3">
