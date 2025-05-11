743af10e29c0ae09c3e0b2c9f148527182e51d1c
Suresh Attanayake <sureshattanayake.a@affinidi.com>
2025-05-11 13:18:06 +0200

    max number of logs to scroll is limited now in the screen.

--- src/Cards/LiveNetworkLogs/DataTable.tsx
+++ src/Cards/LiveNetworkLogs/DataTable.tsx
@@ -5,7 +5,6 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
-  RowPinningState,
   useReactTable,
 } from "@tanstack/react-table";
 import { useNetStore } from "../../stores/net.store";
@@ -22,6 +21,7 @@ interface Props {
 function DataTable({ showTitle }: Props) {
   const [data, setData] = useState<Packet[]>(() => []);
   const [show, setShow] = useState(false);
+  const MAX_ROWS = 100;
 
   const {
     currentInterface,
@@ -146,7 +146,13 @@ function DataTable({ showTitle }: Props) {
         unlisten = await listen("update", (d) => {
           const log = d.payload as Packet;
           if (autoViewNewLog) setSelectedLog(log);
-          setData((prev) => [log, ...prev]);
+          setData((prev) => {
+            const updatedData = [log, ...prev];
+            if (updatedData.length > MAX_ROWS) {
+              updatedData.pop(); 
+            }
+            return updatedData;
+          });
         });
       } else {
         setShow(false);
@@ -161,12 +167,6 @@ function DataTable({ showTitle }: Props) {
       })();
     };
   }, [currentInterface, autoViewNewLog]);
-
-  // useEffect(() => {
-  //   if (autoViewNewLog && data.length > 0) {
-  //     setSelectedLog(data[0]); // Automatically select the latest log
-  //   }
-  // }, [autoViewNewLog, data]);
 
   return show ? (
     <div className="w-full  font-quantic">
@@ -187,42 +187,6 @@ function DataTable({ showTitle }: Props) {
             </tr>
           ))}
         </thead>
-        {/* <tbody className="relative">
-          {data && data.length
-            ? table.getRowModel().rows.map((row) => (
-                <tr
-                  key={row.original.npid}
-                  className={`border-b-1 border-b-white/15   text-sm ${
-                    row.original.npid === selectedLog?.npid
-                      ? `text-[#EDEEEE] ${
-                          !autoViewNewLog
-                            ? "sticky bottom-1 bg-light-green-100 "
-                            : ""
-                        }`
-                      : "text-light-green"
-                  }`}
-                  onClick={() => {
-                    if (selectedLog?.npid == row.original.npid) {
-                      setAutoViewNewLog(true);
-                      return;
-                    }
-                    setAutoViewNewLog(false);
-                    setSelectedLog(row.original);
-                    // row.pin("bottom", false, false);
-                  }}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <td key={cell.id} className="max-w-12 py-2 truncate">
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
-                  ))}
-                </tr>
-              ))
-            : null}
-        </tbody> */}
 
         <tbody className="relative">
           {table.getRowModel().rows.map((row) => (
